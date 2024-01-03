acee3560d4b6c97eb36fa2eed9bdb15d5c89d0d6
Ben Bridts <ben.bridts@vrt.be>
2024-01-03 16:01:15 +0100

    K6 support

--- source/console/src/Components/Details/Details.js
+++ source/console/src/Components/Details/Details.js
@@ -83,6 +83,8 @@ class Details extends React.Component {
       data.method = data.testScenario.scenarios[`${data.testName}`].requests[0].method;
       data.body = data.testScenario.scenarios[`${data.testName}`].requests[0].body;
       data.headers = data.testScenario.scenarios[`${data.testName}`].requests[0].headers;
+    } else {
+      data.testScenario.execution[0].executor = data.testType;
     }
 
     this.setState({
@@ -131,7 +133,14 @@ class Details extends React.Component {
     try {
       const testId = this.state.testId;
       const { testType } = this.state.data;
-      let filename = this.state.data.fileType === "zip" ? `${testId}.zip` : `${testId}.jmx`;
+      var extension;
+      if (testType === "jmeter") {
+        extension = "jmx";
+      }
+      if (testType === "k6") {
+        extension = "js";
+      }
+      let filename = this.state.data.fileType === "zip" ? `${testId}.zip` : `${testId}.${extension}`;
       const url = await Storage.get(`test-scenarios/${testType}/${filename}`, { expires: 10 });
       window.open(url, "_blank");
     } catch (error) {
