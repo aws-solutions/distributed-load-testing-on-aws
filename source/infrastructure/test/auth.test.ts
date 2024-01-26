cc70df716ab6e68201f3c09ba3dca021e5c6f1cc
Bogdan Katishev <bogdan_katishev@hotmail.com>
2024-01-26 10:05:55 +0100

    Update snapshot and tests

--- source/infrastructure/test/auth.test.ts
+++ source/infrastructure/test/auth.test.ts
@@ -20,10 +20,24 @@ test("DLT API Test", () => {
     apiId: "apiId12345",
     cloudFrontDomainName: "test.com",
     scenariosBucketArn: "arn:aws:s3:::DOC-EXAMPLE-BUCKET",
+    existingCognitoPoolId: "",
+  });
+
+  const auth2 = new CognitoAuthConstruct(stack, "TestAuth2", {
+    adminEmail: "test@test.com",
+    adminName: "testname",
+    apiId: "apiId12345",
+    cloudFrontDomainName: "test.com",
+    scenariosBucketArn: "arn:aws:s3:::DOC-EXAMPLE-BUCKET",
+    existingCognitoPoolId: "us-east-1_123456789",
   });
 
   expect(Template.fromStack(stack)).toMatchSnapshot();
   expect(auth.cognitoIdentityPoolId).toBeDefined();
   expect(auth.cognitoUserPoolClientId).toBeDefined();
   expect(auth.cognitoUserPoolId).toBeDefined();
+
+  expect(auth2.cognitoIdentityPoolId).toBeDefined();
+  expect(auth2.cognitoUserPoolClientId).toBeDefined();
+  expect(auth2.cognitoUserPoolId).toBeDefined();
 });
