b7e7b9aff41524a76ed5d44eba5be7496c82a8ad
kamyar ziabari <kamyarz@amazon.com>
2024-09-17 17:52:59 -0400

    removing .d.ts files and js files from metrics-utils/lib

--- source/metrics-utils/lib/query-builders.d.ts
+++
@@ -1,9 +0,0 @@
-import { ILogGroup } from "aws-cdk-lib/aws-logs";
-import { SolutionsMetrics } from "./solutions-metrics";
-export declare function addLambdaInvocationCount(this: SolutionsMetrics, functionName: string, period?: number): void;
-export declare function addCloudFrontMetric(this: SolutionsMetrics, distributionId: string, metricName: string, period?: number): void;
-export declare function addECSAverageCPUUtilization(this: SolutionsMetrics, clusterName: string, taskDefinitionFamily?: string, period?: number): void;
-export declare function addECSAverageMemoryUtilization(this: SolutionsMetrics, clusterName: string, taskDefinitionFamily?: string, period?: number): void;
-export declare function addDynamoDBConsumedWriteCapacityUnits(this: SolutionsMetrics, tableName: string, period?: number): void;
-export declare function addDynamoDBConsumedReadCapacityUnits(this: SolutionsMetrics, tableName: string, period?: number): void;
-export declare function addLambdaBilledDurationMemorySize(this: SolutionsMetrics, logGroups: ILogGroup[], queryDefinitionName: string, limit?: number | undefined): void;
