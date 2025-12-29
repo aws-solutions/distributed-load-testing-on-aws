76f6289372bfdc13af63c8282ce9edc508fc6482
Till Neunast <tilln@users.noreply.github.com>
2025-12-30 11:12:07 +1300

    Inject env var TASK_COUNT

--- source/task-runner/lib/index.spec.js
+++ source/task-runner/lib/index.spec.js
@@ -124,6 +124,7 @@ let mockParam = {
           { name: "FILE_TYPE", value: "none" },
           { name: "LIVE_DATA_ENABLED", value: "live=true" },
           { name: "TIMEOUT", value: calcTimeout(event.testTaskConfig.taskCount) },
+          { name: "TASK_COUNT", value: event.testTaskConfig.taskCount.toString() },
           { name: "PREFIX", value: event.prefix },
           { name: "SCRIPT", value: "ecslistener.py" },
         ],
@@ -269,6 +270,7 @@ describe("#TASK RUNNER:: ", () => {
 
     event.testTaskConfig.taskCount = 20;
     modifyContainerOverrides("TIMEOUT", calcTimeout(event.testTaskConfig.taskCount));
+    modifyContainerOverrides("TASK_COUNT", event.testTaskConfig.taskCount.toString());
     let tasks = getTaskIds(19);
     let expectedResponse = {
       isRunning: true,
@@ -300,7 +302,8 @@ describe("#TASK RUNNER:: ", () => {
     expect(response).toEqual(expect.objectContaining(expectedResponse));
   });
   it("should return when launching leader task is successful", async () => {
-    mockParam.overrides.containerOverrides[0].environment[8].value = "ecscontroller.py";
+    modifyContainerOverrides("SCRIPT", "ecscontroller.py");
+    modifyContainerOverrides("TASK_COUNT", "1");
     mockParam.overrides.containerOverrides[0].environment.push({ name: "IPNETWORK", value: "" });
     let taskIds = getTaskIds(5);
     event.taskIds = taskIds;
