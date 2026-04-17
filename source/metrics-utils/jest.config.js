fc7c125020aaf95ecfd52ad0b4a3d4ed059299f0
David Chung <dvchung@amazon.com>
2026-04-17 11:11:45 -0700

    Revert Makefile and jest.config.js changes

---
+++ source/metrics-utils/jest.config.js
@@ -0,0 +1,11 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+  roots: ["<rootDir>/test"],
+  testMatch: ["**/*.spec.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  coverageReporters: ["text", ["lcov", { projectRoot: "../" }]],
+};
